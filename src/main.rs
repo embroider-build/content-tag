851a9b64457f9556d630f08260e0a65cb1c666d0
Edward Faulkner <edward@eaf4.com>
2023-05-23 16:39:47 -0400

    parse and print

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use std::env;
+use std::{path::Path, process::exit};
 use swc_common::{
     self,
     errors::{ColorConfig, Handler},
     sync::Lrc,
     SourceMap,
 };
-use std::{path::Path, process::exit};
-use std::env;
-use swc_ecma_parser::{lexer::Lexer, Capturing, Parser, StringInput, Syntax};
+use swc_ecma_ast::Module;
+use swc_ecma_codegen::Emitter;
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,14 +17,13 @@ fn main() {
         exit(-1);
     }
     let filename = &args[1];
-    
+
     let cm: Lrc<SourceMap> = Default::default();
     let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
 
     let fm = cm
         .load_file(Path::new(filename))
         .expect("failed to load input file");
-
 
     let lexer = Lexer::new(
         Syntax::Es(Default::default()),
@@ -31,19 +32,39 @@ fn main() {
         None,
     );
 
-    let capturing = Capturing::new(lexer);
+    let mut p = Parser::new_from(lexer);
+    let res = p
+        .parse_module()
+        .map_err(|e| e.into_diagnostic(&handler).emit());
 
-    let mut parser = Parser::new_from(capturing);
-
-    for e in parser.take_errors() {
-        e.into_diagnostic(&handler).emit();
+    for e in p.take_errors() {
+        e.into_diagnostic(&handler).emit()
     }
 
-    let module = parser
-        .parse_module()
-        .map_err(|e| e.into_diagnostic(&handler).emit())
-        .expect("Failed to parse module.");
+    match res {
+        Ok(m) => {println!("{}", print(&m, cm)) }
+        Err(_) => {}
+    }
+}
 
-    //println!("Tokens: {:?}", parser.input().take());
-    println!("{:?}", module );
+fn print(module: &Module, cm: Lrc<SourceMap>) -> String {
+    let mut buf = vec![];
+    {
+        let mut emitter = Emitter {
+            cfg: Default::default(),
+            cm: cm.clone(),
+            wr: Box::new(swc_ecma_codegen::text_writer::JsWriter::new(
+                cm.clone(),
+                "\n",
+                &mut buf,
+                None,
+            )),
+            comments: None,
+        };
+
+        emitter.emit_module(module).unwrap();
+    }
+
+    let s = String::from_utf8_lossy(&buf);
+    s.to_string()
 }
