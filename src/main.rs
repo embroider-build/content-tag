4c05023995e39f6333eead4aee220c4ef66ec6ed
Edward Faulkner <edward@eaf4.com>
2023-05-23 17:50:04 -0400

    beginning to improve library call signature

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
+use glimmer_swc::{gjs_to_js, Options};
 use std::env;
-use std::{path::Path, process::exit};
-use swc_common::{
-    self,
-    errors::{ColorConfig, Handler},
-    sync::Lrc,
-    SourceMap,
-};
-use swc_ecma_ast::Module;
-use swc_ecma_codegen::Emitter;
-use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
-use swc_ecma_visit::{as_folder, FoldWith };
-
-mod transform;
+use std::fs;
+use std::path::PathBuf;
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,59 +10,10 @@ fn main() {
         println!("Must pass input filename");
         exit(-1);
     }
-    let filename = &args[1];
+    let filename: PathBuf = args[1].clone().into();
 
-    let cm: Lrc<SourceMap> = Default::default();
-    let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
+    let src = fs::read_to_string(filename.clone()).unwrap();
 
-    let fm = cm
-        .load_file(Path::new(filename))
-        .expect("failed to load input file");
-
-    let lexer = Lexer::new(
-        Syntax::Es(Default::default()),
-        Default::default(),
-        StringInput::from(&*fm),
-        None,
-    );
-
-    let mut p = Parser::new_from(lexer);
-    let res = p
-        .parse_module()
-        .map_err(|e| e.into_diagnostic(&handler).emit());
-
-    for e in p.take_errors() {
-        e.into_diagnostic(&handler).emit()
-    }
-
-    match res {
-        Ok(m) => {
-            let mut tr = as_folder(transform::TransformVisitor);
-            let mt = m.fold_with(&mut tr);
-            println!("{}", print(&mt, cm)) 
-        }
-        Err(_) => {}
-    }
-}
-
-fn print(module: &Module, cm: Lrc<SourceMap>) -> String {
-    let mut buf = vec![];
-    {
-        let mut emitter = Emitter {
-            cfg: Default::default(),
-            cm: cm.clone(),
-            wr: Box::new(swc_ecma_codegen::text_writer::JsWriter::new(
-                cm.clone(),
-                "\n",
-                &mut buf,
-                None,
-            )),
-            comments: None,
-        };
-
-        emitter.emit_module(module).unwrap();
-    }
-
-    let s = String::from_utf8_lossy(&buf);
-    s.to_string()
+    let output = gjs_to_js(src, Options { filename: Some(filename) }).expect("converted");
+    println!("{}" , output);
 }
