5a61eb8a6490a11070456bb2fe1fa43cbf119064
Chris Manson <chris@manson.ie>
2023-07-07 17:38:38 +0100

    pass a better error when there is a parser error

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,4 +1,11 @@
-use crate::Preprocessor as CorePreprocessor;
+use crate::{Options, Preprocessor as CorePreprocessor};
+use std::{fmt, path::PathBuf, str};
+use swc_common::{
+    errors::Handler,
+    sync::{Lock, Lrc},
+    SourceMap,
+};
+use swc_error_reporters::{GraphicalReportHandler, GraphicalTheme, PrettyEmitter};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -12,6 +19,33 @@ pub struct Preprocessor {
     core: Box<CorePreprocessor>,
 }
 
+#[derive(Clone, Default)]
+struct Writer(Lrc<Lock<String>>);
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.0.lock().write_str(s)
+    }
+}
+
+fn err_to_string(err: swc_ecma_parser::error::Error, source_map: Lrc<SourceMap>) -> String {
+    let wr = Writer::default();
+
+    let emitter = PrettyEmitter::new(
+        source_map,
+        Box::new(wr.clone()),
+        GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor()),
+        Default::default(),
+    );
+
+    let handler = Handler::with_emitter(true, false, Box::new(emitter));
+
+    err.into_diagnostic(&handler).emit();
+
+    let s = wr.0.lock().as_str().to_string();
+    return s.into();
+}
+
 #[wasm_bindgen]
 impl Preprocessor {
     #[wasm_bindgen(constructor)]
@@ -22,8 +56,17 @@ impl Preprocessor {
     }
 
     pub fn process(&self, src: String) -> Result<String, JsValue> {
-        self.core
-            .process(&src, Default::default())
-            .map_err(|_| js_error("Something went wrong".into()))
+        let result = self.core.process(
+            &src,
+            Options {
+                // when we start passing the filename to the preprocessor we will have a better file name here
+                filename: Some(PathBuf::new()),
+            },
+        );
+
+        match result {
+            Ok(output) => Ok(output),
+            Err(err) => Err(js_error(err_to_string(err, self.core.source_map()).into())),
+        }
     }
 }
