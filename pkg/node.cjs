2a143a9b5b0378193f6e80549f04f6ca60f404bf
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-17 18:33:25 -0500

    Prototype transform logic out in JS

--- pkg/node.cjs
+++ pkg/node.cjs
@@ -1,3 +1,54 @@
 const { Preprocessor } = require("./node/content_tag.cjs");
 
 module.exports.Preprocessor = Preprocessor;
+module.exports.transform = function(source, callback)  {
+  let p = new Preprocessor(source);
+
+  // Will be added to or subtracted from depending on the change
+  // in character length each of the transforms
+  let offset = 0;
+  let result = source;
+  let originalSource = source;
+ let buffer = Buffer.from(originalSource, 'utf8');
+
+  let templates = p.parse(source);
+
+  for (let parsed of templates) {
+    let transformed = callback(parsed.content)
+
+    /**
+     * NOTE: range.start w/ range.end is between the <template> and </template>
+     */
+    let originalContent = buffer.slice(parsed.range.start, parsed.range.end).toString();
+    let originalLength = originalContent.length;
+    let originalBeforeContent = buffer.slice(0, parsed.range.start).toString();
+    let originalStart = originalBeforeContent.length;
+    let originalEnd = originalStart + originalLength;
+
+    /**
+     * Need to make sure the opening <template> and closing </template>
+     * are not removed.
+     *
+     * We aren't just using the strings <template> and </template>, because
+     * its possible for the opening <template ..... > to have attributes in the future
+     * with futher syntax extensions
+     * - Signature
+     * - defaults?
+     * - macros?
+    */
+    let openingTag = buffer.slice(parsed.startRange.start, parsed.startRange.end).toString();
+    let closingTag = buffer.slice(parsed.endRange.start, parsed.endRange.end).toString();
+
+    result =
+      result.slice(0, originalStart + offset) +
+      openingTag +
+      transformed +
+      closingTag +
+      result.slice(originalEnd + offset, result.length);
+
+    offset = transformed.length - originalLength;
+  }
+
+  return result;
+
+}
---
+++ test/node/transform.test.js
@@ -0,0 +1,29 @@
+import chai from "chai";
+import { codeEquality } from "code-equality-assertions/chai";
+import { transform } from "content-tag";
+
+chai.use(codeEquality);
+
+const { expect } = chai;
+
+describe(`transform`, () => {
+  it('works', () => {
+    const gjs = [
+      "test('it renders', async (assert) => {",
+      '  await render(<template>',
+      '  <div class="parent">',
+      '    <div class="child"></div>',
+      '  </div>',
+      '  </template>);',
+      '});',
+    ].join('\n');
+
+    let result = transform(gjs, (hbs) => 'replaced!');
+
+    expect(result).to.deep.equal([
+      "test('it renders', async (assert) => {",
+      '  await render(<template>replaced!</template>);',
+      '});',
+    ].join('\n'));
+  });
+});
