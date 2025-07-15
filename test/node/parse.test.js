f6fa36a26959a918eed65ab8c8074610381867a7
Patrick Pircher <patricklx@users.noreply.github.com>
2025-07-15 10:25:29 +0200

    use utf 16 code points

--- test/node/parse.test.js
+++ test/node/parse.test.js
@@ -17,15 +17,15 @@ describe(`parse`, function () {
         type: "expression",
         tagName: "template",
         contents: "Hello!",
-        range: { startByte: 0, endByte: 27, startChar: 0, endChar: 27 },
+        range: { startByte: 0, endByte: 27, startUtf16Codepoint: 0, endUtf16Codepoint: 27 },
         contentRange: {
           startByte: 10,
           endByte: 16,
-          startChar: 10,
-          endChar: 16,
+          startUtf16Codepoint: 10,
+          endUtf16Codepoint: 16,
         },
-        startRange: { startByte: 0, endByte: 10, startChar: 0, endChar: 10 },
-        endRange: { startByte: 16, endByte: 27, startChar: 16, endChar: 27 },
+        startRange: { startByte: 0, endByte: 10, startUtf16Codepoint: 0, endUtf16Codepoint: 10 },
+        endRange: { startByte: 16, endByte: 27, startUtf16Codepoint: 16, endUtf16Codepoint: 27 },
       },
     ]);
   });
@@ -38,15 +38,15 @@ describe(`parse`, function () {
         type: "expression",
         tagName: "template",
         contents: "Hello!",
-        range: { startByte: 12, endByte: 39, startChar: 12, endChar: 39 },
+        range: { startByte: 12, endByte: 39, startUtf16Codepoint: 12, endUtf16Codepoint: 39 },
         contentRange: {
           startByte: 22,
           endByte: 28,
-          startChar: 22,
-          endChar: 28,
+          startUtf16Codepoint: 22,
+          endUtf16Codepoint: 28,
         },
-        startRange: { startByte: 12, endByte: 22, startChar: 12, endChar: 22 },
-        endRange: { startByte: 28, endByte: 39, startChar: 28, endChar: 39 },
+        startRange: { startByte: 12, endByte: 22, startUtf16Codepoint: 12, endUtf16Codepoint: 22 },
+        endRange: { startByte: 28, endByte: 39, startUtf16Codepoint: 28, endUtf16Codepoint: 39 },
       },
     ]);
   });
@@ -63,15 +63,15 @@ describe(`parse`, function () {
         type: "class-member",
         tagName: "template",
         contents: "Hello!",
-        range: { startByte: 25, endByte: 52, startChar: 25, endChar: 52 },
+        range: { startByte: 25, endByte: 52, startUtf16Codepoint: 25, endUtf16Codepoint: 52 },
         contentRange: {
           startByte: 35,
           endByte: 41,
-          startChar: 35,
-          endChar: 41,
+          startUtf16Codepoint: 35,
+          endUtf16Codepoint: 41,
         },
-        startRange: { startByte: 25, endByte: 35, startChar: 25, endChar: 35 },
-        endRange: { startByte: 41, endByte: 52, startChar: 41, endChar: 52 },
+        startRange: { startByte: 25, endByte: 35, startUtf16Codepoint: 25, endUtf16Codepoint: 35 },
+        endRange: { startByte: 41, endByte: 52, startUtf16Codepoint: 41, endUtf16Codepoint: 52 },
       },
     ]);
   });
@@ -90,15 +90,15 @@ describe(`parse`, function () {
         type: "expression",
         tagName: "template",
         contents: "Hello!",
-        range: { startByte: 41, endByte: 68, startChar: 41, endChar: 68 },
+        range: { startByte: 41, endByte: 68, startUtf16Codepoint: 41, endUtf16Codepoint: 68 },
         contentRange: {
           startByte: 51,
           endByte: 57,
-          startChar: 51,
-          endChar: 57,
+          startUtf16Codepoint: 51,
+          endUtf16Codepoint: 57,
         },
-        startRange: { startByte: 41, endByte: 51, startChar: 41, endChar: 51 },
-        endRange: { startByte: 57, endByte: 68, startChar: 57, endChar: 68 },
+        startRange: { startByte: 41, endByte: 51, startUtf16Codepoint: 41, endUtf16Codepoint: 51 },
+        endRange: { startByte: 57, endByte: 68, startUtf16Codepoint: 57, endUtf16Codepoint: 68 },
       },
     ]);
   });
@@ -114,15 +114,15 @@ describe(`parse`, function () {
         type: "expression",
         tagName: "template",
         contents: "Hello!",
-        range: { startByte: 43, endByte: 70, startChar: 43, endChar: 70 },
+        range: { startByte: 43, endByte: 70, startUtf16Codepoint: 43, endUtf16Codepoint: 70 },
         contentRange: {
           startByte: 53,
           endByte: 59,
-          startChar: 53,
-          endChar: 59,
+          startUtf16Codepoint: 53,
+          endUtf16Codepoint: 59,
         },
-        startRange: { startByte: 43, endByte: 53, startChar: 43, endChar: 53 },
-        endRange: { startByte: 59, endByte: 70, startChar: 59, endChar: 70 },
+        startRange: { startByte: 43, endByte: 53, startUtf16Codepoint: 43, endUtf16Codepoint: 53 },
+        endRange: { startByte: 59, endByte: 70, startUtf16Codepoint: 59, endUtf16Codepoint: 70 },
       },
     ]);
   });
@@ -153,15 +153,15 @@ describe(`parse`, function () {
         type: "expression",
         tagName: "template",
         contents: "Hello!",
-        range: { startByte: 34, endByte: 61, startChar: 32, endChar: 59 },
+        range: { startByte: 34, endByte: 61, startUtf16Codepoint: 32, endUtf16Codepoint: 59 },
         contentRange: {
           startByte: 44,
           endByte: 50,
-          startChar: 42,
-          endChar: 48,
+          startUtf16Codepoint: 42,
+          endUtf16Codepoint: 48,
         },
-        startRange: { startByte: 34, endByte: 44, startChar: 32, endChar: 42 },
-        endRange: { startByte: 50, endByte: 61, startChar: 48, endChar: 59 },
+        startRange: { startByte: 34, endByte: 44, startUtf16Codepoint: 32, endUtf16Codepoint: 42 },
+        endRange: { startByte: 50, endByte: 61, startUtf16Codepoint: 48, endUtf16Codepoint: 59 },
       },
     ]);
   });
@@ -184,27 +184,17 @@ describe(`parse`, function () {
     {
       let { range, startRange, endRange, contentRange } = one;
 
-      expect(slice(range.startChar, range.endChar)).to.eql(
-        `<template>💩💩💩💩💩💩💩</template>`,
-      );
-      expect(slice(startRange.startChar, startRange.endChar)).to.eql(
-        `<template>`,
-      );
+      expect(slice(range.startChar, range.endChar)).to.eql(`<template>💩💩💩💩💩💩💩</template>`);
+      expect(slice(startRange.startChar, startRange.endChar)).to.eql(`<template>`);
       expect(slice(endRange.startChar, endRange.endChar)).to.eql(`</template>`);
-      expect(slice(contentRange.startChar, contentRange.endChar)).to.eql(
-        `💩💩💩💩💩💩💩`,
-      );
+      expect(slice(contentRange.startChar, contentRange.endChar)).to.eql(`💩💩💩💩💩💩💩`);
     }
 
     {
       let { range, startRange, endRange, contentRange } = two;
 
-      expect(slice(range.startChar, range.endChar)).to.eql(
-        `<template>💩</template>`,
-      );
-      expect(slice(startRange.startChar, startRange.endChar)).to.eql(
-        `<template>`,
-      );
+      expect(slice(range.startChar, range.endChar)).to.eql(`<template>💩</template>`);
+      expect(slice(startRange.startChar, startRange.endChar)).to.eql(`<template>`);
       expect(slice(endRange.startChar, endRange.endChar)).to.eql(`</template>`);
       expect(slice(contentRange.startChar, contentRange.endChar)).to.eql(`💩`);
     }
